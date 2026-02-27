9d65d6af9eac0ed26a42965f59d44f817298b431
LRD678 <84108202+LRD678@users.noreply.github.com>
2026-02-27 08:44:49 -0500

    Project created

---
+++ Test/test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Student on 2026-02-27.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
